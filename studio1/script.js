f151bf5533eec37a6ac0e38f6cb940a18d3c6b7e
TimotheaW <trwang@ucdavis.edu>
2018-01-28 17:41:53 -0800

    Fixed html page
    Checked HTML validation and fixed the errors.

--- studio1/script.js
+++ studio1/script.js
@@ -10,6 +10,7 @@ document.f.onsubmit=processForm;
 document.f.onreset=resetForm;
 
 function processForm() {
+  results.setAttribute('class','show');
 
   var userName= document.f.userName.value;
   var noun1= document.f.noun1.value;
@@ -24,7 +25,6 @@ function processForm() {
   msg.innerHTML = "Hello " + userName + ", I stumbled upon your portfolio and I love your work. Could you design a " + noun1 +" for me? I've searched all over for a " + designSpecialization + " designer to work with and I think your style is the one I’ve been looking for. <br> I want it to look like the design by " + famousCompany+ ", but don’t copy it, just make it different enough but keep it the same! I was thinking we could also use comic sans for a fun, casual vibe. <br> I’ll pay you "+ number + " in cash for the job. We need this done ASAP, so just start and figure it out on your own, yeah?<br> Thanks! <br> Bad "+color+" "+animal+"<br> Representative of "+noun2+" " +noun3;
 
   document.getElementById("container").style.top="-1000px";
-  results.setAttribute('class','show');
 
   return false;
 }
