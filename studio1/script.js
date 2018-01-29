636092160e3998989e9d64ca98980ec94ee4f4ee
TimotheaW <trwang@ucdavis.edu>
2018-01-28 22:51:20 -0800

    Added CSS focus and hover elements
    Changed <title> and CSS input:hover and button styles.

--- studio1/script.js
+++ studio1/script.js
@@ -27,8 +27,6 @@ function processForm() {
 
   msg.innerHTML = "Hello <strong>" + userName + "</strong>, I stumbled upon your portfolio and I love your work. Could you design a <strong>" + noun1 +"</strong> for me? I've searched all over for a <strong>" + designSpecialization + "</strong> designer to work with and I think your style is the one I’ve been looking for. I want it to look like the popular design by <strong>" + famousCompany+ "</strong>, but don’t copy it, just make it different enough but keep it the same!<br><br> I was thinking we could also use comic sans for a fun, casual, and trendy vibe. I’ll pay you <strong>$"+ num + "</strong> in cash for the job. We need this done ASAP, so just start and figure it out on your own, yeah?<br><br> Thanks! <br> Bad <strong>"+color+" "+animal+"</strong> <br> Representative of <strong>"+noun2+" " +noun3+"</strong>";
 
-  // document.getElementById("container").style.top="-1000px";
-
   return false;
 }
 
