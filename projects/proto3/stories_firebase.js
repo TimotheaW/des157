355b73679c175b4d0b21cb3313ba162e7f84305c
TimotheaW <trwang@ucdavis.edu>
2018-03-12 16:55:14 -0700

    prototype 3 upload

--- projects/proto3/stories_firebase.js
+++ projects/proto3/stories_firebase.js
@@ -10,17 +10,7 @@ firebase.initializeApp(config);
 
 var values;
 
-//reference messages collection
-// var messagesRef = firebase.database().ref('messages');
-// messagesRef.on('value', function(snapshot){
-//   //updateMessages(postElement, snapshot.val());
-//   console.log(snapshot.val());
-//   values = snapshot.val();
-// });
 
-// function updateMessages (){
-
-// }
 //how to tie in with 3D --> add event listeners on the particles and let them retrieve 
 // a random post from the database
 
@@ -32,14 +22,17 @@ newMessageRef.on('value', function (snapshot) {
   // stores the value of the ref as a js Object
   console.log('val:');
   values = snapshot.val();
-  console.log(values);
+  console.log(values); // prints values to console
 
   // iterates through children of the ref
   console.log('forEach:');
   snapshot.forEach(function (childSnapshot) {
     console.log(childSnapshot.key);
     console.log(childSnapshot.val());
-  });
+    document.getElementById("demo").innerHTML = childSnapshot.key;
+    document.getElementById("demo").innerHTML = "<br />" + childSnapshot.val();
+  }); // for each child, print the key and value to console
+
 
   //test
   // var value = values[Math.random()];
